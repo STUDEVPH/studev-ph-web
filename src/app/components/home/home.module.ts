861a1ad964639790c8483830e694c3ff2c22b84a
jerick <jericksignapan3331@gmail.com>
2023-08-06 14:57:46 +0800

    feat: :sparkles: import who we are component

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LocationComponent } from 'src/app/shared/location/location.component';
+import { WhoWeAreComponent } from 'src/app/shared/who-we-are/who-we-are.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule,LocationComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    LocationComponent,
+    WhoWeAreComponent,
+  ],
 })
 export class HomeModule {}
