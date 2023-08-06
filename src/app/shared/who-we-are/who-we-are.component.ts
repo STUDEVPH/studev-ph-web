2cf8ed39e83aa859e3f3296be8bef46e8ab868b8
jerick <jericksignapan3331@gmail.com>
2023-08-06 15:53:37 +0800

    feat: :sparkles: create wo are we page

---
+++ src/app/shared/who-we-are/who-we-are.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ICarousel, ISelect } from 'src/app/interfaces';
+
+@Component({
+  selector: 'who-we-are',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './who-we-are.component.html',
+  styleUrls: ['./who-we-are.component.scss'],
+})
+export class WhoWeAreComponent {
+  logo = '../../../assets/png/logo-big.png';
+  selectedIndex: number = 0;
+
+  selected: ISelect[] = [
+    { selected: '', active: true },
+    { selected: '', active: false },
+    { selected: '', active: false },
+    { selected: '', active: false },
+  ];
+
+  controls: ICarousel[] = [
+    {
+      title: 'GWAPO NG DEVELOPER',
+      description:
+        'All of this text is editable. Simply click anywhere in the paragraph or heading text and start typing. You can copy and paste your own content into see what it look like with these font combinations.',
+    },
+    {
+      title: 'HINDI AKO NAG BIBIRO',
+      description:
+        'All of this text is editable. Simply click anywhere in the paragraph or heading text and start typing. You can copy and paste your own content into see what it look like with these font combinations.',
+    },
+    {
+      title: 'ANO BA GWAPO NGA AKO',
+      description:
+        'All of this text is editable. Simply click anywhere in the paragraph or heading text and start typing. You can copy and paste your own content into see what it look like with these font combinations.',
+    },
+    {
+      title: 'HAYS GWAPO NGA AKO',
+      description:
+        'All of this text is editable. Simply click anywhere in the paragraph or heading text and start typing. You can copy and paste your own content into see what it look like with these font combinations.',
+    },
+  ];
+
+  setSelected(control: ISelect, index: number): void {
+    this.selected.forEach((item) => {
+      item.active = false;
+    });
+
+    control.active = true;
+    this.selectedIndex = index;
+  }
+
+  isCarouselVisible(index: number): boolean {
+    return this.selectedIndex === index;
+  }
+}
