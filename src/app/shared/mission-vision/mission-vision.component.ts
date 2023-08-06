e6ffc72ee45d1265a2b213f906244cb4afcf0fc0
jerick <jericksignapan3331@gmail.com>
2023-08-06 18:54:44 +0800

    feat: :sparkles: create mission vision page

---
+++ src/app/shared/mission-vision/mission-vision.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mission-vision',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './mission-vision.component.html',
+  styleUrls: ['./mission-vision.component.scss']
+})
+export class MissionVisionComponent {
+
+}
