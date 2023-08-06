ab3cead5bbf3ecd87485087840c0ae15ff2c3426
jerick <jericksignapan3331@gmail.com>
2023-08-06 16:48:05 +0800

    feat: add IFollowers interface

---
+++ src/app/interface/index.ts
@@ -0,0 +1,15 @@
+export interface ISelect {
+  selected: string;
+  active: boolean;
+}
+
+export interface ICarousel {
+  title: string;
+  description: string;
+}
+
+export interface IFollowers {
+  box: string;
+  total: string;
+  location: string;
+}
